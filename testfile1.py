72c05ae46931ab05126ac8f7374ded35ec28eb65
Rajkumar1711 <rajkumarwgl55@gmail.com>
2025-07-01 09:27:44 +0530

    code review

--- testfile1.py
+++ testfile1.py
@@ -23,7 +23,7 @@ def main():
     choice = input("Enter your choice (1/2/3/4): ")
 
     if choice not in ('1', '2', '3', '4'):
-        print("Invalid input")
+        pritn("Invalid input")
         return
 
     try:
